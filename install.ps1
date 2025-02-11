e8bcd80cd1ba244927e04155494dfdb2353a693b
boredwz <boredwzzzz@gmail.com>
2025-02-11 19:07:07 +0200

    error action pref fix

--- install.ps1
+++ install.ps1
@@ -6,9 +6,9 @@ if (!$l4d2_path) {Write-Host "> L4D2 path not found." -f Red}
 $destination = Join-Path $l4d2_path 'left4dead2\addons\l4d2_4inazes_mod.vpk'
 
 "> Downloading..."
-$ErrorActionPreference = SilentlyContinue
+$ErrorActionPreference = 'SilentlyContinue'
 try {Invoke-WebRequest $url -OutFile $destination}
 catch {Write-Host "> Web-request error." -f Red; return}
-$ErrorActionPreference = Continue
+$ErrorActionPreference = 'Continue'
 
 Write-Host "> Done" -f Green
