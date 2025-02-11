5472a9ebaf51675d4a8d04592e65a7b3da03305c
boredwz <boredwzzzz@gmail.com>
2025-02-11 19:08:25 +0200

    error action -> progress setting

--- install.ps1
+++ install.ps1
@@ -6,9 +6,9 @@ if (!$l4d2_path) {Write-Host "> L4D2 path not found." -f Red}
 $destination = Join-Path $l4d2_path 'left4dead2\addons\l4d2_4inazes_mod.vpk'
 
 "> Downloading..."
-$ErrorActionPreference = 'SilentlyContinue'
+$ProgressPreference = 'SilentlyContinue'
 try {Invoke-WebRequest $url -OutFile $destination}
 catch {Write-Host "> Web-request error." -f Red; return}
-$ErrorActionPreference = 'Continue'
+$ProgressPreference = 'Continue'
 
 Write-Host "> Done" -f Green
