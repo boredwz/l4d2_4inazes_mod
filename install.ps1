6b72525f00dd40bbb59984fa186d5a40148a9a91
boredwz <boredwzzzz@gmail.com>
2025-02-11 18:38:01 +0200

    init

---
+++ install.ps1
@@ -0,0 +1,8 @@
+$url = 'https://github.com/l4d2_4inazes_mod/releases/latest/download/l4d2_4inazes_mod.vpk'
+
+$l4d2_reg = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Steam App 550'
+$l4d2_path = (Get-ItemProperty $l4d2_reg -ErrorAction 0).InstallLocation
+if (!$l4d2_path) {return "> L4D2 path not found."}
+$addons_path = Join-Path $l4d2_path 'left4dead2\addons'
+
+Invoke-WebRequest $url -OutFile $addons_path
